d344850a34564d55e508771225771ff462f0e913
Laurin Muth <l.muth@lyniat.games>
2021-02-03 21:44:50 +0100

    Create dictionary.js

---
+++ content/dictionary.js
@@ -0,0 +1,7 @@
+const dictionary = {
+  "dnd":["dnd","5th","SRD","OGL"],
+  "kickstarter":["kickstarter","crowdfunding","funding","buy","support"],
+  "release":["release","start","2021","21","June","July"],
+  "platform":["platform","windows","microsoft","mac","macos","osx","linux","ubuntu","iOS","iPadOS","Apple","iPhone","iPad","iPod","Android","smartphone","tablet","mobile","desktop","app"],
+  "lyniat-location":["location","europe","germany","bavaria","ratisbona","regensburg","city","country","land","district","state"]
+};
