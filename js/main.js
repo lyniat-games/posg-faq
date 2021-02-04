b04e3464cb15cb40d844a588c3688086fdc4f8a3
Laurin Muth <l.muth@lyniat.games>
2021-02-04 13:17:51 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+const siteUrl = "https://faq.savage-gods.com";
+
 let input = document.getElementById("input-query");
   input.addEventListener('input', function (evt) {
     onInput(input.value);
   });
+
+addShareButtons();
   
   let resetButton = document.getElementById("btn-reset");
   resetButton.addEventListener('click', function (evt) {
     reset();
   });
   
-  let urlQuery = window.location.hash.substr(1); <!-- myurl.com/#test => test -->
+  let urlQuery = window.location.hash.substr(1); // myurl.com/#test => test
   if(urlQuery){
       setInputFromUrl(urlQuery);
   }
@@ -20,12 +24,40 @@ let input = document.getElementById("input-query");
   }
 
   let foundEntries = 0;
+
+function addShareButtons(){
+  let aqs = document.getElementsByClassName("aq");
+  Array.from(aqs).forEach(element => {
+      const link = document.createElement('p');
+      link.className = 'btn-share';
+      element.appendChild(link);
+      link.addEventListener('click', function (evt) {
+        copyToClipboard(element);
+       });
+    });
+}
+
+function copyToClipboard(element){
+    let tags = element.getAttribute("tags");
+    let id = tags.split(",")[0];
+    //if nobody did a mistake, first tag should be question number
+    document.getElementById("ghost-input").value = siteUrl + "/#" + id; 
+    var copyText = document.getElementById("ghost-input");
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    document.execCommand("copy");
+}
   
   function onInput(value){
     if(value === ""){
       reset();
       return;
     }
+    
+    //reset ghost input
+    document.getElementById("ghost-input").value = "";
     
     foundEntries = 0;
     
