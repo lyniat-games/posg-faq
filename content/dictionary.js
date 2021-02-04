93b522ac1843a4beba8428b0e2feee5457e53a88
Laurin Muth <l.muth@lyniat.games>
2021-02-04 14:04:03 +0100

    Update dictionary.js

--- content/dictionary.js
+++ content/dictionary.js
@@ -1,7 +1,9 @@
 const dictionary = {
   "dnd":["dnd","5th","SRD","OGL"],
   "kickstarter":["kickstarter","crowdfunding","funding","buy","support"],
-  "release":["release","start","2021","21","June","July"],
-  "platform":["platform","windows","microsoft","mac","macos","osx","linux","ubuntu","iOS","iPadOS","Apple","iPhone","iPad","iPod","Android","smartphone","tablet","mobile","desktop","app"],
-  "lyniat-location":["location","europe","germany","bavaria","ratisbona","regensburg","city","country","land","district","state"]
+  "release":["release","start","2021","21","June","July","alpha","beta","version"],
+  "platform":["platform","version","windows","microsoft","mac","macos","osx","linux","ubuntu","iOS","iPadOS","Apple","iPhone","iPad","iPod","Android","smartphone","tablet","mobile","desktop","app"],
+  "lyniat-location":["location","europe","germany","bavaria","ratisbona","regensburg","city","country","land","district","state"],
+  "engine":["engine","unity","unity3d","programming","shader"],
+  "community":["community","post","posts","newsletter","alpha","beta","test","testing","question","questions","answer","answers","aq","faq","discord","chat","chats","voice","meet","meeting"]
 };
