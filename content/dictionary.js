dca1cb0893253724bb4efba8f9b5aef3f5cbf99c
Laurin Muth <l.muth@lyniat.games>
2021-02-04 14:28:20 +0100

    Update dictionary.js

--- content/dictionary.js
+++ content/dictionary.js
@@ -5,5 +5,6 @@ const dictionary = {
   "platform":["platform","version","windows","microsoft","mac","macos","osx","linux","ubuntu","iOS","iPadOS","Apple","iPhone","iPad","iPod","Android","smartphone","tablet","mobile","desktop","app"],
   "lyniat-location":["location","europe","germany","bavaria","ratisbona","regensburg","city","country","land","district","state"],
   "engine":["engine","unity","unity3d","programming","shader"],
+  "modding":["modding","mod","modification","customize","edit","change","modify","code","coding","unity","unity3d","custom","content","sprite","sprites","graphic","graphics","tile","tiles","tilemap","tilemaps","map","maps","script","scripting","shader","render","rendering"],
   "community":["community","post","posts","newsletter","alpha","beta","test","testing","question","questions","answer","answers","aq","faq","discord","chat","chats","voice","meet","meeting"]
 };
